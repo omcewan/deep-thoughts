7bb436d8fc8a38a08fe26cb1a0ee66bab900e571
omcewan <orlandomcewan231@gmail.com>
2022-04-27 23:19:19 -0400

    set up user types and queries along with resolvers

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -6,6 +6,26 @@ const resolvers = {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
+
+    // place this inside of the `Query` nested object right after `thoughts`
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
   },
 };
 
